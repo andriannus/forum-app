fcb435fc9ae67944b895f00874e23f065aef70f7
Andriannus Parasian <andriannus.parasian@efishery.com>
2023-03-28 23:36:19 +0700

    feat: install and setup Notyf

---
+++ src/context/notyf/notyf.context.ts
@@ -0,0 +1,10 @@
+import { Notyf } from "notyf";
+import { createContext, useContext } from "react";
+
+const NotyfContext = createContext(
+  new Notyf({
+    duration: 2000,
+  }),
+);
+
+export const useNotyf = () => useContext(NotyfContext);
