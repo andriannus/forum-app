4c8b1b94f0e21d871b8c82339c8f863eee915e7a
Andriannus Parasian <andriannus.parasian@efishery.com>
2023-03-29 11:52:37 +0700

    feat: create `TextArea` component

---
+++ src/components/textarea/textarea.component.tsx
@@ -0,0 +1,91 @@
+import PropTypes from "prop-types";
+import { ChangeEvent, ChangeEventHandler, forwardRef, useEffect } from "react";
+
+import "./textarea.component.scss";
+
+interface TextAreaProps {
+  autoCapitalize: string;
+  autoComplete: string;
+  className: string;
+  disabled: boolean;
+  id: string;
+  name: string;
+  onChange: ChangeEventHandler<HTMLTextAreaElement>;
+  placeholder: string;
+  readOnly: boolean;
+  value: string;
+}
+
+const TextArea = forwardRef<HTMLTextAreaElement, Partial<TextAreaProps>>(
+  (
+    {
+      autoCapitalize = "",
+      autoComplete = "",
+      className = "",
+      disabled = false,
+      id = "",
+      name = "",
+      onChange = () => null,
+      placeholder = "",
+      readOnly = false,
+      value = "",
+    },
+    ref,
+  ) => {
+    useEffect(() => {
+      handleAutoResize();
+    }, []);
+
+    function handleAutoResize(): void {
+      const elements =
+        document.querySelectorAll<HTMLTextAreaElement>("textarea");
+      const equalizingNumber = 2;
+
+      elements.forEach((element) => {
+        element.style.resize = "none";
+        element.style.height = "auto";
+        element.style.height = `${element?.scrollHeight + equalizingNumber}px`;
+      });
+    }
+
+    function handleOnChange(event: ChangeEvent<HTMLTextAreaElement>): void {
+      handleAutoResize();
+      onChange(event);
+    }
+
+    return (
+      <div className={className}>
+        <textarea
+          ref={ref}
+          id={id}
+          autoCapitalize={autoCapitalize}
+          autoComplete={autoComplete}
+          className="TextArea-input"
+          disabled={disabled}
+          name={name}
+          placeholder={placeholder}
+          readOnly={readOnly}
+          title={placeholder}
+          value={value}
+          onChange={handleOnChange}
+        />
+      </div>
+    );
+  },
+);
+
+TextArea.displayName = "TextArea";
+TextArea.propTypes = {
+  autoCapitalize: PropTypes.string,
+  autoComplete: PropTypes.string,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  readOnly: PropTypes.bool,
+  value: PropTypes.string,
+};
+
+export default TextArea;
