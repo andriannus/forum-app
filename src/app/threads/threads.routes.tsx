953a9f5171d102f1f9ce3152bc2d873fc6bd875e
Andriannus Parasian <andriannus.parasian@efishery.com>
2023-03-29 09:51:41 +0700

    feat: guard threads routes

--- src/app/threads/threads.routes.tsx
+++ src/app/threads/threads.routes.tsx
@@ -1,13 +1,19 @@
+import { lazy } from "react";
 import { RouteObject } from "react-router-dom";
 
+import { Authenticated } from "@/app/auth/components";
 import { MobileWithBottomNav } from "@/layouts";
 
-import { Threads } from "../threads";
+const Threads = lazy(() => import("./threads.component"));
 
 export const threadsRoutes: RouteObject[] = [
   {
     path: "",
-    element: <MobileWithBottomNav />,
+    element: (
+      <Authenticated>
+        <MobileWithBottomNav />
+      </Authenticated>
+    ),
     children: [
       {
         index: true,
