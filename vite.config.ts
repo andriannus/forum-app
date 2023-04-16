1ef132f612968a95610c7773d2109ffcc99a5c32
Andriannus Parasian <andriannus.parasian@efishery.com>
2023-04-16 19:23:34 +0700

    feat: install and setup unit test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -19,5 +20,13 @@ export default defineConfig({
         replacement: path.resolve(__dirname, "."),
       },
     ],
+  },
+  test: {
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "json", "html"],
+    },
+    environment: "jsdom",
+    globals: true,
   },
 });
